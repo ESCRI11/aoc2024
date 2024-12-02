a022e73ff36361d11390a0dcac7b471575adb65d
ESCRI11 <xavier.escriba.montagut@gmail.com>
2024-12-02 16:22:02 +0100

    .

--- 1/2.R
+++ 1/2.R
@@ -2,11 +2,9 @@ input <- data.table::fread("input")
 input <- data.frame(input)
 
 left_uq <- unique(input[,1] |> unlist())
-lapply(left_uq, function(x) {
+score <- lapply(left_uq, function(x) {
   n_right <- sum(input[,2] %in% x)
   n_left <- sum(input[,1] %in% x)
   return(n_left * n_right * x)
-})
-input[,1] <- sort(input[,1] |> unlist())
-input[,2] <- sort(input[,2] |> unlist())
-sum(abs(input[,1] - input[,2]))
+}) |> unlist()
+sum(score)
