e97a6d00cc7189847bf81aaad241863f95f35ae9
ESCRI11 <xavier.escriba.montagut@gmail.com>
2024-12-03 09:53:54 +0100

    .

--- 3/1.R
+++ 3/1.R
@@ -1,6 +1,5 @@
 input <- readLines("input")
 input <- paste(input, sep = "", collapse = "")
-regexpr
 muls <- regmatches(input, gregexpr("mul\\(\\d+,\\d+\\)", input))[[1]]
 multiplier <- function(mul) {
   nums <- stringr::str_match(mul, "mul\\((\\d+),(\\d+)\\)")[,2:3]
